1ed25b1cf3302f8b53867ff5769e26dbcae6a4ff
rohityalavarthy <Rohit.yalavarthy@gmail.com>
2025-09-27 19:50:13 +0530

    First commit - local testibf

---
+++ mcp_server.py
@@ -0,0 +1,16 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
